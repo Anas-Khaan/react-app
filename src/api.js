aeb9b9eeda082586cec95e585b0d3df1958a587d
Anas-Khaan <anaskhangiki@gmail.com>
2023-10-31 00:16:08 +0500

    weather app funtionalities completed

---
+++ src/api.js
@@ -0,0 +1,13 @@
+
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '2845d0395fmsh8150aa234316c24p125b1bjsn281c59611d8e',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+
+export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5'
+export const WEATHER_API_KEY = '72dd39d3f6871d70f71b39065436ec3b'
