09ab61c8078ab9a917ddf625a2a2d947c99c8eae
Anas-Khaan <anaskhangiki@gmail.com>
2023-10-31 00:45:01 +0500

    some minor changes

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@
 export const geoApiOptions = {
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Key': '2845d0395fmsh8150aa234316c24p125b1bjsn281c59611d8e',
+		'X-RapidAPI-Key': 'XXXXXXX',
 		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
 	}
 };
@@ -10,4 +10,4 @@ export const geoApiOptions = {
 export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
 
 export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5'
-export const WEATHER_API_KEY = '72dd39d3f6871d70f71b39065436ec3b'
+export const WEATHER_API_KEY = 'XXXXXXXXXXXXXXXXXSS'
